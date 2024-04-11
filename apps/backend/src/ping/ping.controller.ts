b169ac11716eea35b30f61f227a898a3cab09a06
Lucas Costa <francisco.lucas0507@gmail.com>
2024-04-11 10:48:11 -0300

    drizzle + turso

--- apps/backend/src/ping/ping.controller.ts
+++ apps/backend/src/ping/ping.controller.ts
@@ -1,43 +1,7 @@
-import {
-  Controller,
-  Get,
-  Injectable,
-  Param,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { IsNumber, IsString } from 'class-validator';
-
-export class Ponger {
-  @IsNumber()
-  id: number;
-  @IsString()
-  ponger: string;
-}
-
-@Injectable()
-export class PingService {
-  private map: Map<number, string> = new Map();
-
-  push(id: number): string {
-    this.map.set(id, new Date().toISOString());
-    return this.map.get(id);
-  }
-  get(id: number): string | undefined {
-    return this.map.get(id);
-  }
-  entries() {
-    return Array.from(this.map.entries());
-  }
-}
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { PingService } from './ping.service';
 
 @Controller('ping')
-@UsePipes(
-  new ValidationPipe({
-    whitelist: true,
-  }),
-)
 export class PingController {
   constructor(private readonly pingService: PingService) {}
   @Get()
