ca43c184c32ae27eecad1e446d39938382a6d544
Lucas Costa <francisco.lucas0507@gmail.com>
2024-04-12 13:12:18 -0300

    fix: remove database from global context

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -1,7 +1,6 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DatabaseProvider } from './database.provider';
 
-@Global()
 @Module({
   imports: [],
   providers: [DatabaseProvider],
